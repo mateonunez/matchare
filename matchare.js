085f75e706e31267f1ed7a1cfb13c5303554725e
mateonunez <mateonunez95@gmail.com>
2024-08-27 18:55:12 +0200

    chore: the simplest way
    Signed-off-by: mateonunez <mateonunez95@gmail.com>

--- matchare.js
+++ matchare.js
@@ -1,4 +1,34 @@
-import { matchare } from './src/core.js'
+export function matchare (actual, expected) {
+  if (typeof actual === 'string' && typeof expected === 'string') {
+    const patterns = expected.trim().split(/\r?\n/).map(function trimLine (line) {
+      return line.trim()
+    })
 
-export { matchare }
-export default matchare
+    let lastIndex = -1
+    for (const pattern of patterns) {
+      const index = actual.indexOf(pattern)
+      if (index === -1 || index < lastIndex) {
+        return false
+      }
+      lastIndex = index
+    }
+    return true
+  }
+
+  for (const key in expected) {
+    if (key in actual) {
+      if (typeof expected[key] === 'object' && expected[key] !== null) {
+        if (!matchare(actual[key], expected[key])) {
+          return false
+        }
+      } else {
+        if (actual[key] !== expected[key]) {
+          return false
+        }
+      }
+    } else {
+      return false
+    }
+  }
+  return true
+}
